dea52837a239af75c5853468dd4ccd8b440659fa
Emerson Soares da Silva <esds.emerson1993@gmail.com>
2024-05-24 16:46:06 -0300

    Criando classes.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Clients c001 = new Clients();
+        c001.firstName = "Emerson";
+        c001.age = 29;
+
+        Clients c002 = new Clients();
+        c002.firstName = "Eveny";
+        c002.age = 26;
+
+        c002.newAccount();
+
+    }
+}
